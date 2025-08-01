dea17f532e346948b5145e3e2184f6df5f99155d
Jose Gabriel <guardiao230503@gmail.com>
2025-08-01 09:25:37 -0300

    to learn c++

--- learning/codes/if&&.cpp
+++ learning/codes/if&&.cpp
@@ -4,3 +4,17 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+    int x = 1;
+    int y = 2;
+
+    if ((x <= y) && (y >= x)) {
+        cout << "x value is <= y and y >= x\n";
+    }
+    else {
+        cout << "x value is >= y and y <= x \n";
+    }
+    //OR
+    //condition in line -> ternary operator
+    cout << (((x >= y) && (y <= x)) ? "x is >= y AND x is <= x" : "x is not >= y AND x is not <= x\n");
+}
